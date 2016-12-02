a4c36b1506cf3436eb6fa5788108891aeab3489d
rodrigjg <rodrigjg@ucalgary.ca>
2016-12-02 01:26:20 -0700

    Added embedded yt vid

--- Convergr.NET/Convergr.NET/App_Start/BundleConfig.cs
+++ Convergr.NET/Convergr.NET/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Convergr.NET
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-paper.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Convergr.NET/Convergr.NET/Models/YouTubeVideo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Convergr.NET.Models
+{
+    public class YouTubeVideo
+    {
+        public int Id { get; set; }
+        public string VideoTitle { get; set; }
+    }
+    
+}
