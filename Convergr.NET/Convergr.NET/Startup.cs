5b8deb74aa16df0c94b1b6f4535c8b04a6594b13
rodrigjg <rodrigjg@ucalgary.ca>
2016-12-02 00:05:22 -0700

    Project start and setup

---
+++ Convergr.NET/Convergr.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Convergr.NET.Startup))]
+namespace Convergr.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
