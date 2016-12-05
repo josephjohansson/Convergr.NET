d2c6fdacab987e888cab76739a5c0bacdbf026c0
Jose Rodriguez <rodrigjg@ucalgary.ca>
2016-12-05 10:26:05 -0700

    fixing

---
+++ Convergr.NET/Convergr.NET/Models/Hashtag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convergr.NET.Models
+{
+    public class Hashtag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Convergr.NET/Convergr.NET/ViewModels/RandomVideoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Convergr.NET.Models;
+
+namespace Convergr.NET.ViewModels
+{
+    class RandomVideoViewModel
+    {
+        public YouTubeVideo Video { get; set; }
+        public List<Hashtag> Hashtags { get; set; }
+
+    }
+}
